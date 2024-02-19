2fe9ee72bf510b1d073a8c8c84407dc689896974
Pavan Naik <82699802+pavannaik321@users.noreply.github.com>
2024-02-19 18:36:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,13 @@ const connection = require("./db");
 const Products = require("./models/products");
 const cookieParser = require("cookie-parser");
 
+// Use the CORS middleware
+app.use(cors({
+    credentials: true,
+    origin: ["https://levitaion-frontend.vercel.app"],
+    methods:["POST","GET","PUT"],
+  }));
+
 //database connection
 connection();
 
@@ -14,12 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-// Use the CORS middleware
-app.use(cors({
-    credentials: true,
-    origin: ["https://levitaion-frontend.vercel.app"],
-    methods:["POST","GET","PUT"],
-  }));
+
 
 app.use("/", require("./routes/authRout"));
 app.use("/product", require("./routes/productRout"));
