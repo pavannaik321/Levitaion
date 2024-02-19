24953928bcca40706017f9f4174d931317b5269f
Pavan Naik <82699802+pavannaik321@users.noreply.github.com>
2024-02-19 18:34:09 +0530

    Update invoiceRout.js

--- backend/routes/invoiceRout.js
+++ backend/routes/invoiceRout.js
@@ -4,13 +4,13 @@ const cors = require("cors");
 const { InvoiceDetails } = require("../controllers/InvoiceController");
 
 // middleware
-router.use(
-  cors({
-    credentials: true,
-    origin: ["https://levitaion-frontend.vercel.app"],
-    methods:["POST","GET","PUT"],
-  })
-);
+// router.use(
+//   cors({
+//     credentials: true,
+//     origin: ["https://levitaion-frontend.vercel.app"],
+//     methods:["POST","GET","PUT"],
+//   })
+// );
 
 // routes
 router.get("/:userId", InvoiceDetails);
